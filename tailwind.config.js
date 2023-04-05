e812394e6854e0df201da0ea1e9f440d70348b90
kyuki3rain <kyuki3rain@gmail.com>
2023-04-05 15:28:55 +0000

    微調整

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,6 @@ module.exports = {
   plugins: [
     require("@tailwindcss/typography"),
     require("@tailwindcss/forms"),
-    require("@tailwindcss/line-clamp"),
   ],
   theme: {
     extend: {},
